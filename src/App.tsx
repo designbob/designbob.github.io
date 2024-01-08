e973d02a7704e6e7555a60bdafa837bf5874e360
DesignBob <58170798+designbob@users.noreply.github.com>
2024-01-08 20:28:09 +0000

    Set type styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import "./styles/base.scss";
 function App() {
 	return (
 		<>
-			<p>DesignBob</p>
+			<h1>DesignBob</h1>
+			<p>A <a href="#">paragraph Lorem ipsum</a> dolor sit amet, consectetur adipisicing elit. Nihil quo inventore magnam sint, odit ut temporibus eveniet mollitia omnis perferendis quod exercitationem dolor dicta enim, obcaecati quas, suscipit asperiores aperiam?</p>
+			<h2>Heading 2</h2>
+			<p>A paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quo inventore magnam sint, odit ut temporibus eveniet mollitia omnis perferendis quod exercitationem dolor dicta enim, obcaecati quas, suscipit asperiores aperiam?</p>
+			<h3>Heading 3</h3>
+			<p>A paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quo inventore magnam sint, odit ut temporibus eveniet mollitia omnis perferendis quod exercitationem dolor dicta enim, obcaecati quas, suscipit asperiores aperiam?</p>
+			<h4>Heading 4</h4>
+			<p>A paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quo inventore magnam sint, odit ut temporibus eveniet mollitia omnis perferendis quod exercitationem dolor dicta enim, obcaecati quas, suscipit asperiores aperiam?</p>
+			<h5>Heading 5</h5>
+			<p>A paragraph Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quo inventore magnam sint, odit ut temporibus eveniet mollitia omnis perferendis quod exercitationem dolor dicta enim, obcaecati quas, suscipit asperiores aperiam?</p>
+			<p><small>This is some small text</small></p>
 		</>
 	);
 }
